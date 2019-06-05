570e3a7fcc60cf6c8bc97d2e7bcaa171e5d91bfd
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2019-06-05 15:26:56 +0200

    [HOPSWORKS-1110] Operator to administer a model serving instance (#20)
    [HOPSWORKS-1110] Update, start and stop a model serving instance
    
    [HOPSWORKS-1110] New API
    
    [HOPSWORKS-1110] Add flag to make Launch operator blocking

--- files/default/hopsworks_plugin/operators/__init__.py
+++ files/default/hopsworks_plugin/operators/__init__.py
@@ -1,7 +1,9 @@
 from hopsworks_plugin.operators.hopsworks_operator import HopsworksLaunchOperator
 from hopsworks_plugin.operators.hopsworks_operator import HopsworksSqoopOperator
+from hopsworks_plugin.operators.hopsworks_operator import HopsworksModelServingInstance
 
 HOPSWORKS_OPERATORS = [
     HopsworksLaunchOperator,
-    HopsworksSqoopOperator
+    HopsworksSqoopOperator,
+    HopsworksModelServingInstance
 ]
